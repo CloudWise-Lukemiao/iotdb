662183c5ccdbfd703f449b3491857a028c4cb928
luke.miao <luke.miao@yunzhihui.com>
2022-04-06 17:03:19 +0800

    add go env check

--- grafana-plugin/backend-compile.bat
+++ grafana-plugin/backend-compile.bat
@@ -1,27 +1,32 @@
-#/bin/bash
-#
-# Licensed to the Apache Software Foundation (ASF) under one
-# or more contributor license agreements.  See the NOTICE file
-# distributed with this work for additional information
-# regarding copyright ownership.  The ASF licenses this file
-# to you under the Apache License, Version 2.0 (the
-# "License"); you may not use this file except in compliance
-# with the License.  You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing,
-# software distributed under the License is distributed on an
-# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
-# KIND, either express or implied.  See the License for the
-# specific language governing permissions and limitations
-# under the License.
-#
-go version
+@REM
+@REM Licensed to the Apache Software Foundation (ASF) under one
+@REM or more contributor license agreements.  See the NOTICE file
+@REM distributed with this work for additional information
+@REM regarding copyright ownership.  The ASF licenses this file
+@REM to you under the Apache License, Version 2.0 (the
+@REM "License"); you may not use this file except in compliance
+@REM with the License.  You may obtain a copy of the License at
+@REM
+@REM     http://www.apache.org/licenses/LICENSE-2.0
+@REM
+@REM Unless required by applicable law or agreed to in writing,
+@REM software distributed under the License is distributed on an
+@REM "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+@REM KIND, either express or implied.  See the License for the
+@REM specific language governing permissions and limitations
+@REM under the License.
+@REM
+
+@echo off
+set EXISTS_ENV=`go version|findstr -c "go version"`
+IF  "%EXISTS_ENV%" == "0" (
+echo "Need to install go environment"
+exit
+)
 go get -u github.com/grafana/grafana-plugin-sdk-go
 go mod tidy
 go env
-check_results=`go env |grep GOPATH= | sed 's/\"//g'`
-go_path=${check_results/GOPATH=/}
-go run $go_path/pkg/mod/github.com/magefile/mage@v1.13.0/bootstrap.go
+set CHECK_RESULTS =`go env |findstr GOPATH= | sed 's/\"//g'`
+go_path="%CHECK_RESULTS%"
+go run "%CHECK_RESULTS%"/pkg/mod/github.com/magefile/mage@v1.13.0/bootstrap.go
 $go_path/bin/mage -v
