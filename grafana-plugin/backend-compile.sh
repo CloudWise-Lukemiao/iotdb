0c7bf0735468bc749a183d3478dd201fe147ddf7
luke.miao <luke.miao@yunzhihui.com>
2022-04-15 17:20:37 +0800

    fixed ci fail

--- grafana-plugin/backend-compile.sh
+++ grafana-plugin/backend-compile.sh
@@ -30,7 +30,7 @@ go_path=${check_results/GOPATH=/}
 chmod 755 $go_path/pkg/mod/github.com/magefile/*
 go run $go_path/pkg/mod/github.com/magefile/mage@v1.13.0/bootstrap.go
 go env
-cd $go_path/go/bin/
+cd $go_path/
 pwd
 ls
-$go_path//bin/mage -v
+$go_path/bin/mage -v
