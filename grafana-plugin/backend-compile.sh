bbea707195f6ae75f4d5a2c197586d7b4a76f75f
luke.miao <luke.miao@yunzhihui.com>
2022-04-15 18:05:17 +0800

    fixed ci fail

--- grafana-plugin/backend-compile.sh
+++ grafana-plugin/backend-compile.sh
@@ -22,14 +22,13 @@ if [ $exists_env -eq 0 ];then
 echo "Need to install go environment"
 exit 1
 fi
+pwd
 go env -w GOPROXY=https://goproxy.cn
 go get -u github.com/grafana/grafana-plugin-sdk-go
 go mod tidy
 check_results=`go env |grep GOPATH= | sed 's/\"//g'`
 go_path=${check_results/GOPATH=/}
+cd $go_path/pkg/mod/github.com/magefile/mage@v1.13.0
 chmod 755 $go_path/pkg/mod/github.com/magefile/*
 go run $go_path/pkg/mod/github.com/magefile/mage@v1.13.0/bootstrap.go
-go env
-cd $go_path/pkg/mod/github.com/magefile/mage@v1.13.0/
-ls
-$go_path/pkg/mod/github.com/magefile/mage@v1.13.0/mage -v
+$go_path/bin/mage -v
