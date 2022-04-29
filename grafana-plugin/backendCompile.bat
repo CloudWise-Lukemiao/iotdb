ecdf6102e3e10f17902cb91fd1ae8873bc4502be
luke.miao <luke.miao@yunzhihui.com>
2022-04-29 11:34:47 +0800

    test

--- grafana-plugin/backendCompile.bat
+++ grafana-plugin/backendCompile.bat
@@ -27,7 +27,7 @@ exit
 go get -u github.com/grafana/grafana-plugin-sdk-go
 go mod tidy
 go env
-set CHECK_RESULTS =`go env |findstr GOPATH=`
+set CHECK_RESULTS =`go env GOPATH=`
 cd "%CHECK_RESULTS%"/pkg/mod/github.com/magefile/mage@v1.13.0/bootstrap.go
 dir
 go run bootstrap.go
